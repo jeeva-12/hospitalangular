8685c027aacc647af0d72a62f550c1abad65b695
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-24 11:53:07 +0530

    search

---
+++ src/app/patiet-search/patiet-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patiet-search',
+  templateUrl: './patiet-search.component.html',
+  styleUrls: ['./patiet-search.component.css']
+})
+export class PatietSearchComponent {
+
+}
