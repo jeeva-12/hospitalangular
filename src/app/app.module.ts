c58234bdc2760cff9973cbe94478349cba4875e1
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-24 12:46:43 +0530

    rout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,26 @@ import { AppComponent } from './app.component';
 import { PatientEntryComponent } from './patient-entry/patient-entry.component';
 import { PatietSearchComponent } from './patiet-search/patiet-search.component';
 import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
+import { Route, RouterModule, RouterState, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:PatientEntryComponent
+  },
+  {
+    path:"Search",
+    component:PatietSearchComponent
+  },
+  {
+    path:"delete",
+    component:PatientDeleteComponent
+  },
+
+]
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +36,8 @@ import { PatientDeleteComponent } from './patient-delete/patient-delete.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
